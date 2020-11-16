39a1e562d7b7053cc244d8773c988c1ce66291a8
Milan Tepic <97-2018@ftn.edu.rs>
2020-11-16 15:05:50 +0100

    Code.js added with tables for compute, jump and destination mnemonics

---
+++ assembler/Code.js
@@ -0,0 +1,76 @@
+class Code{
+  dest(mnemonic){
+    const destTable = [
+      {name:"M", value:"001"},
+      {name:"D", value:"010"},
+      {name:"MD", value:"011"},
+      {name:"A", value:"100"},
+      {name:"AM", value:"101"},
+      {name:"AD", value:"110"},
+      {name:"AMD", value:"111"}];
+      
+    if(destTable.find(x => x.name === mnemonic)){
+      return destTable.find(x => x.name === mnemonic).value;
+    }
+    else{
+      return '000';
+    }
+
+  }
+  comp(mnemonic){
+    const compTable = [
+      {name:"0", value:"0101010"},
+      {name:"1", value:"0111111"},
+      {name:"-1", value:"0111010"},
+      {name:"D", value:"0001100"},
+      {name:"A", value:"0110000"},
+      {name:"!D", value:"0001101"},
+      {name:"!A", value:"0110001"},
+      {name:"-D", value:"0001111"},
+      {name:"-A", value:"0110011"},
+      {name:"D+1", value:"0011111"},
+      {name:"A+1", value:"0110111"},
+      {name:"D-1", value:"0001110"},
+      {name:"A-1", value:"0110010"},
+      {name:"D+A", value:"0000010"},
+      {name:"D-A", value:"0010011"},
+      {name:"A-D", value:"0000111"},
+      {name:"D&A", value:"0000000"},
+      {name:"D|A", value:"0010101"},
+      {name:"M", value:"1110000"},
+      {name:"!M", value:"1110001"},
+      {name:"-M", value:"1110011"},
+      {name:"M+1", value:"1110111"},
+      {name:"M-1", value:"1110010"},
+      {name:"D+M", value:"1000010"},
+      {name:"D-M", value:"1010011"},
+      {name:"M-D", value:"1000111"},
+      {name:"D&M", value:"1000000"},
+      {name:"D|M", value:"1010101"}];
+
+      if(compTable.find(x => x.name == mnemonic)){
+        return compTable.find(x => x.name == mnemonic).value;
+      }
+      else{
+        return '000000';
+      }
+
+  }
+  jump(mnemonic){
+    const jumpTable = [
+      {name:"JGT", value:"001"},
+      {name:"JEQ", value:"010"},
+      {name:"JGE", value:"011"},
+      {name:"JLT", value:"100"},
+      {name:"JNE", value:"101"},
+      {name:"JLE", value:"110"},
+      {name:"JMP", value:"111"}];
+      
+    if(jumpTable.find(x => x.name === mnemonic)){
+      return jumpTable.find(x => x.name === mnemonic).value;
+    }
+    else{
+      return '000';
+    }
+  }
+}
